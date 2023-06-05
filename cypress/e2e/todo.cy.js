6545a1361e0eed1ac07c33ffbd5618eba226ca84
erik-lance <eriklance@gmail.com>
2023-06-05 21:29:41 +0800

    Added urls to the other pages needed

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,6 +1,13 @@
 describe('Test connection', () => {
   it('Visit Todo Site', () => {
-    cy.visit(Cypress.env('host'))
+    cy.visit(Cypress.env('todo'))
   })
-})
 
+  it('Visit Actions Site', () => {
+    cy.visit(Cypress.env('commandActions'))
+  })
+
+  it('Visit Misc Site', () => {
+    cy.visit(Cypress.env('commandMisc'))
+  }
+})
