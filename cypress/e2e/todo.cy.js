acc66113e503b245e5a8ca606d558d92f5e7e049
erik-lance <eriklance@gmail.com>
2023-06-05 21:51:45 +0800

    Added initial add test

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -11,3 +11,19 @@ describe('Test connection', () => {
     cy.visit(Cypress.env('commandMisc'))
   })
 })
+
+describe('Basic Add Tests', () => {
+  beforeEach( () => {
+    cy.visit(Cypress.env('todo'))
+    cy.get('.todo-list li').should('have.length', 2)
+
+  })
+
+  it('Add a new todo', () => {
+    cy.getDataByTestId('new-todo').type('Walk the dog{enter}')
+    
+    // Check the text of the new todo
+    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+  })
+
+})
