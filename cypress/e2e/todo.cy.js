73b34c54946988a87f47e17009f038581c1279b5
erik-lance <eriklance@gmail.com>
2023-06-05 21:51:55 +0800

    Auto formatted

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,29 +1,27 @@
-describe('Test connection', () => {
-  it('Visit Todo Site', () => {
-    cy.visit(Cypress.env('todo'))
-  })
+describe("Test connection", () => {
+	it("Visit Todo Site", () => {
+		cy.visit(Cypress.env("todo"));
+	});
 
-  it('Visit Actions Site', () => {
-    cy.visit(Cypress.env('commandActions'))
-  })
+	it("Visit Actions Site", () => {
+		cy.visit(Cypress.env("commandActions"));
+	});
 
-  it('Visit Misc Site', () => {
-    cy.visit(Cypress.env('commandMisc'))
-  })
-})
+	it("Visit Misc Site", () => {
+		cy.visit(Cypress.env("commandMisc"));
+	});
+});
 
-describe('Basic Add Tests', () => {
-  beforeEach( () => {
-    cy.visit(Cypress.env('todo'))
-    cy.get('.todo-list li').should('have.length', 2)
+describe("Basic Add Tests", () => {
+	beforeEach(() => {
+		cy.visit(Cypress.env("todo"));
+		cy.get(".todo-list li").should("have.length", 2);
+	});
 
-  })
+	it("Add a new todo", () => {
+		cy.getDataByTestId("new-todo").type("Walk the dog{enter}");
 
-  it('Add a new todo', () => {
-    cy.getDataByTestId('new-todo').type('Walk the dog{enter}')
-    
-    // Check the text of the new todo
-    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
-  })
-
-})
+		// Check the text of the new todo
+		cy.get(".todo-list li").last().should("have.text", "Walk the dog");
+	});
+});
