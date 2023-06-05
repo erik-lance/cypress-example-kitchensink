3401fb5fd219bd2f19c049a972c37515982951ff
erik-lance <eriklance@gmail.com>
2023-06-05 21:54:01 +0800

    Renamed to work with specs

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -12,15 +12,15 @@ describe("Test connection", () => {
 	});
 });
 
-describe("Basic Add Tests", () => {
+describe("Todo Tests", () => {
 	beforeEach(() => {
 		cy.visit(Cypress.env("todo"));
 		cy.get(".todo-list li").should("have.length", 2);
+
+    cy.getDataByTestId("new-todo").type("Walk the dog{enter}");
 	});
 
-	it("Add a new todo", () => {
-		cy.getDataByTestId("new-todo").type("Walk the dog{enter}");
-
+	it("Check if added todo item exists", () => {
 		// Check the text of the new todo
 		cy.get(".todo-list li").last().should("have.text", "Walk the dog");
 	});
